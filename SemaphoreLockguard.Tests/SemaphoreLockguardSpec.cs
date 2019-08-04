32b21871bf1bd567f03c2fe97ff89f8a4b501a22
심승용 <seungyong.shim@mirero.co.kr>
2019-08-04 14:36:23 +0900

    SemaphoreLockguard 라이브러리 작성

---
+++ SemaphoreLockguard.Tests/SemaphoreLockguardSpec.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using FluentAssertions;
+using Xunit;
+
+namespace SemaphoreLockguard.Tests
+{
+    public class SemaphoreLockguardSpec
+    {
+        [Fact]
+        public void Should_Be_Create_Semaphore_One()
+        {
+            // Arrange
+            var sema = new Semaphore(1, 1);
+
+            // Act
+            using (var semalock = new SemaphoreLockguard(sema, TimeSpan.FromSeconds(1)))
+            {
+                // Assert
+                semalock.IsGotHandle.Should().BeTrue();
+            }
+        }
+
+        [Fact]
+        public void Should_Be_Create_Semaphore_More_Than_One_False()
+        {
+            // Arrange
+            var sema = new Semaphore(1, 1);
+            sema.WaitOne();
+
+            // Act
+            using (var semalock = new SemaphoreLockguard(sema, TimeSpan.FromSeconds(1)))
+            {
+                // Assert
+                semalock.IsGotHandle.Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ SemaphoreLockguard/SemaphoreLockguard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace SemaphoreLockguard
+{
+    public class SemaphoreLockguard : IDisposable
+    {
+        public Semaphore Semaphore { get; }
+        public bool IsGotHandle { get; }
+
+
+        public SemaphoreLockguard(Semaphore semaphore, TimeSpan timeout)
+        {
+            Semaphore = semaphore;
+            IsGotHandle = semaphore.WaitOne(timeout);
+        }
+        
+        #region IDisposable Support
+
+        public bool IsDisposed { get; private set; } = false;
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDisposed)
+            {
+                if (disposing)
+                {
+                    if (IsGotHandle == true)
+                    {
+                        Semaphore.Release();
+                    }
+                }
+
+                IsDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
